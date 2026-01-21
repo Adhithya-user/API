b591a739e5c880442534f74dfa912c0993f04ca7
Adhithya_Ramesh <154457229+Adhithya-user@users.noreply.github.com>
2026-01-21 23:56:05 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+from tkinter import *
 import requests
+import os
 
-response=requests.get(url="http://api.open-notify.org/iss-now.json")
+def get_quote():
+    response= requests.get("https://api.kanye.rest/")
+    response.raise_for_status()
+    data=response.json()
+    quote=data["quote"]
+    canvas.itemconfig(quote_text,text=quote)
 
-print(response)
-print(response.json())
+window = Tk()
+window.title("Adhithya Says...")
+window.config(padx=50, pady=50)
 
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file=os.path.join(os.path.dirname(__file__), "background.png"))
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="In upcoming future most people will affect Eyesight Problems", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
 
-"""import requests
+kanye_img = PhotoImage(file=os.path.join(os.path.dirname(__file__), "kanye.png"))
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
 
-response = requests.get("http://api.open-notify.org/iss-now.json")
-print(response.status_code)
-print(response.json())"""
+window.mainloop()
